ff72caf60a4a101b15780cd1e4fb4d665603c72a
Shaza Hamdan <shaza.hamdan@gmail.com>
2024-12-15 00:21:08 +0300

    Recognize project structure and set up tests

---
+++ SampleProject.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SampleProject.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void TestAddition()
+    {
+        int result = 2 + 2;
+        Assert.Equal(4, result);
+    }
+}
